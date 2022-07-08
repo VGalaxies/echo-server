dfcca6f43be41fcbe0e43cc2c3a82f54829d079a
V_Galaxy <1904821183@qq.com>
2022-07-08 16:15:18 +0800

    test: simple unit test

--- src/main/java/vgalaxy/echoserver/exception/TooManyRequestsException.java
+++ src/main/java/vgalaxy/echoserver/exception/TooManyRequestsException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.TOO_MANY_REQUESTS, reason="Too many requests")  // 429
-public class TooManyRequestsException extends RuntimeException{
+public class TooManyRequestsException extends RuntimeException {
 }
--- src/test/java/vgalaxy/echoserver/EchoServerApplicationTests.java
+++ src/test/java/vgalaxy/echoserver/EchoServerApplicationTests.java
@@ -1,13 +1,21 @@
 package vgalaxy.echoserver;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import vgalaxy.echoserver.model.Echo;
+import vgalaxy.echoserver.service.EchoService;
 
 @SpringBootTest
 class EchoServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    EchoService echoService;
 
+    @Test
+    public void echoTest() {
+        Echo res = echoService.echo("world");
+        Assertions.assertEquals(res.getContent(), "Hello, world!");
+    }
 }
